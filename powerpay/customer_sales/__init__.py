023cebafe44d8c5e086ab58193caa32b2d076f03
Jeff Tarimo <92029584+Tarimo077@users.noreply.github.com>
2024-11-25 19:02:54 +0300

    Delete redundant directory

--- powerpay/customer_sales/__init__.py
+++
@@ -1,3 +0,0 @@
-# customer_sales/__init__.py
-
-default_app_config = 'customer_sales.apps.CustomerSalesConfig'
--- powerpay/customer_sales/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomerSalesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'customer_sales'
