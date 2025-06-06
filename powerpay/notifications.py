750f8f21f60be7c53cb8d6c18745970bd34a8e93
Jeff Tarimo <tarimojeff@gmail.com>
2025-06-06 22:12:20 +0300

    added responsive mode for landing page and time handling for notifications

--- powerpay/notifications.py
+++ powerpay/notifications.py
@@ -1,17 +1,22 @@
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-import json
-from datetime import datetime
+from django.utils.timezone import now
+import pytz
 
-def send_notification(user, title , message):
+def send_notification(user, title, message):
     """Function to send real-time notifications to a user"""
     channel_layer = get_channel_layer()
+    
+    # Use East Africa Time (EAT)
+    eat = pytz.timezone('Africa/Nairobi')
+    timestamp = now().astimezone(eat).strftime("%Y-%m-%d %H:%M:%S")
+    
     async_to_sync(channel_layer.group_send)(
         f"user_{user.id}_notifications",
         {
             "type": "send_notification",
             "message": message,
-            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "timestamp": timestamp,
             "title": title,
         }
     )
