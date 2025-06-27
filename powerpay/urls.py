fc696bc424414d33bda6c64b8edc81316e4db088
Jeff Tarimo <tarimojeff@gmail.com>
2025-06-27 10:59:51 +0300

    security added to remove indexed pages

--- powerpay/urls.py
+++ powerpay/urls.py
@@ -1,26 +1,10 @@
-"""
-URL configuration for powerpay project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -60,6 +44,7 @@ urlpatterns = [
     path('mec_downloads/', views.mec_downloads, name='mec_downloads'),
     path('export_mec_downloads/', views.export_mec_downloads, name='export_mec_downloads'),
     path('export_single_mec_download/', views.mec_single_download, name='export_mec_downloads_single'),
+    path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
